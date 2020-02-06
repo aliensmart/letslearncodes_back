6d91cac3a03cbaef9c6a10c34ee2667cd2c6a75d
aliensmart <kaoua17@gmail.com>
2020-02-06 03:03:19 -0500

    email configured

---
+++ flask_app/route.py
@@ -0,0 +1,35 @@
+from flask import jsonify, request, Flask
+from flask_mail import Message, Mail
+from flask_cors import CORS
+import os
+
+
+app = Flask(__name__)
+cors = CORS(app)
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com" #gmail smtp settings
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get('DB_USER')
+app.config["MAIL_PASSWORD"] = os.environ.get('DB_PASS')
+
+mail.init_app(app)
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    data = request.get_json()
+    print(data)
+    print(os.environ.get('DB_USER'))
+
+    name = data['name']
+    email = data['email']
+    subject = data['subject']
+    message = data['message']
+    print(name)
+    msg = Message(subject, sender=email, recipients=[os.environ.get('DB_USER')])
+    msg.body = """
+    From: {} <{}>
+    {}
+    """.format(name, email, message)
+    mail.send(msg)
